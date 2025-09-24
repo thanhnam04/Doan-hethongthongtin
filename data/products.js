994ed7ac7a1da96eec9bfc1d2c5271f01283a55e
thanhnam04 <nnam62673@gmail.com>
2025-09-24 13:57:50 +0700

    up

--- data/products.js
+++ data/products.js
@@ -2,7 +2,7 @@ var list_products = [{
     "name": "SamSung Galaxy J4+",
     "company": "Samsung",
     "img": "img/products/samsung-galaxy-j4-plus-pink-400x400.jpg",
-    "price": "3.490.000",
+    "price": "2.490.000",
     "star": 3,
     "rateCount": 26,
     "promo": {
